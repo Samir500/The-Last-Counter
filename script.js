9237a19d347e2251fc1eb0d1da1cc35065fd8ced
ThefriendlyNeighbourhoodCoder <akshpatra0412personal@gmail.com>
2023-02-14 14:30:40 +0530

    --> Blogs

--- script.js
+++ script.js
@@ -386,3 +386,30 @@ var swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
     }
 });
+
+var testimonials = document.getElementById('testimonials');
+var control1 = document.getElementById('control1');
+var control2 = document.getElementById('control2');
+var control3 = document.getElementById('control3');
+
+
+control1.onclick = function() {
+    testimonials.style.transform = "translateX(870px)";
+    control1.classList.add("active");
+    control2.classList.remove("active");
+    control3.classList.remove("active");
+}
+
+control2.onclick = function() {
+    testimonials.style.transform = "translateX(0px)";
+    control1.classList.remove("active");
+    control2.classList.add("active");
+    control3.classList.remove("active");
+}
+
+control3.onclick = function() {
+    testimonials.style.transform = "translateX(-870px)";
+    control1.classList.remove("active");
+    control2.classList.remove("active");
+    control3.classList.add("active");
+}
